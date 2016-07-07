45bedef6761aed18b2503d5b9fef2b995241d82a
maibaduy <maibaduy@gmail.com>
2016-07-07 16:47:36 +0700

    Move modules\system sang package viisystem\common

---
+++ backend/Module.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace viisystem\common\backend;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'viisystem\common\backend\controllers';
+
+//    public function init()
+//    {
+//        parent::init();
+//    }
+}
---
+++ frontend/Module.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace viisystem\common\frontend;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'viisystem\common\frontend\controllers';
+
+//    public function init()
+//    {
+//        parent::init();
+//    }
+}
